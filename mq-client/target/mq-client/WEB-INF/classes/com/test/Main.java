fa21cc9d6ddbd48fdafedfffb96345c48c4498d4
zhaoweixs <zhaoweixs@163.com>
2017-11-26 16:05:14 +0800

    client

---
+++ mq-client/target/mq-client/WEB-INF/classes/com/test/Main.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author
+ * @create 2017-11-26 16:02
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaa");
+    }
+}
